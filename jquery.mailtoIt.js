000b425d933ea880b6a5e4a237f5a8ec9d4f55b7
chris fuchman <chris-fuchman@gogdog.com>
2015-12-27 14:23:49 -0500

    2nd Commit

--- jquery.mailtoIt.js
+++ jquery.mailtoIt.js
@@ -1,1 +1,37 @@
- 
+/*
+ * name: MailtoIt
+ *
+ * version: 0.1.0
+ *
+ */ 
+
+$(function($){
+	
+	$.fn.mailtoIt = function(options){
+	
+		// def settings
+		var settings = $.extend({
+			mailto: null,
+			text: null
+			
+		},options);
+		
+		//console.log(this.html());	
+		//console.log(settings.whatever);	
+		
+		// validation
+		if(settings.mailto==null) console.log('Missing mailto option');
+		
+		return this.each(function(){
+		
+			var object=$(this);
+			if(settings.text==null) settings.text=object.text();	
+
+			object.wrap('<a href="mailto:'+settings.mailto+'"></a>').text(settings.text);
+			
+		});
+		
+		
+	}
+	
+}(jQuery)); 
